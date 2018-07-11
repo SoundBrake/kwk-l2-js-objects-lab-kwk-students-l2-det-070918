516630920c0d95667a87979ea43ad6d86279bfcc
SoundBrake <mesuper222@gmail.com>
2018-07-11 19:02:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution in this file!
+var driver = {};
+
+function updateDriverWithKeyAndValue(d, k, v){
+  var newDriver = Object.assign({}, d)
+  newDriver[k] = v
+  return newDriver
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(d, k, v){
+  d[k] = v
+  return d
+}
+
+function deleteFromDriverByKey(d, k){
+  var newDriver = Object.assign({}, d)
+  
+}
